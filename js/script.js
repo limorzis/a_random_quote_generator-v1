ce6c981e1f75d4d01d26e8390e40548718e12830
limorzis <lzisbrod@gmail.com>
2020-07-13 08:57:27 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+var quotes = [
+    {
+       quote: 'Yes, I killed Yvette. I hated her so much, it was like fl- flames, flames, on the side of my face, bre-, hea-, heaving, breathless',
+       source: 'Mrs. White',
+       citation: 'Clue',
+       year: 1985
+    },
+    {
+       quote: 'Thank God for the model trains, you know? If they didn\'t have the model trains they wouldn\'t have gotten the idea for the big trains.',
+       source: 'Amber Cole',
+       citation: 'A Mighty Wind',
+       year: 2003
+    },
+    {
+       quote: 'Kill the Lawyer!',
+       source: 'Rufio',
+       citation: 'Hook',
+       year: 1991
+    }
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
