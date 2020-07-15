d99de9ee8b833547928efc1687aa582f40794f7f
limorzis <lzisbrod@gmail.com>
2020-07-15 00:35:52 -0400

    Extra credit added

--- js/script.js
+++ js/script.js
@@ -22,19 +22,22 @@ var quotes = [
        quote: 'Thank God for the model trains, you know? If they didn\'t have the model trains they wouldn\'t have gotten the idea for the big trains.',
        source: 'Amber Cole',
        citation: 'A Mighty Wind',
-       year: 2003
+       year: 2003,
+       tag: 'Mechanical Engineering'
     },
     {
        quote: 'We all wear masks, metaphorically speaking.',
        source: 'Dr. Arthur Neuman',
        citation: 'The Mask',
-       year: 1994
+       year: 1994,
+       tag: 'Philosophy'
     },
     {
        quote: 'Kill the Lawyer!',
        source: 'Rufio',
        citation: 'Hook',
-       year: 1991
+       year: 1991,
+       tag: 'Humor'
     }
     ,
     {
@@ -50,74 +53,65 @@ var quotes = [
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
-  // 1. Create a variable that generates a random number
-  // between zero and the last index in the `quotes` array
+  // `randomNumber` variable generates a random number between zero and the last index in the `quotes` array
   const randomNumber = Math.floor( Math.random() * quotes.length );
 
-  // 2. Use the random number variable and bracket notation 
-  // to grab a random object from the `quotes` array, and 
-  // store it in a variable
+  // Grab a random object from the `quotes` array and store it in variable `selectedQuote`
   const selectedQuote = (quotes[randomNumber]);
 
-  // 3. Return the variable storing the random quote object
-        // console.log(randomNumber);
-        // console.log(quotes[randomNumber]);
-        // console.log(selectedQuote);  
-  // console.log(selectedQuote);
+  // Return `selectedQuote` 
   return selectedQuote;
 }
-
-// getRandomQuote()
-// const randomQuote = getRandomQuote();
-// console.log(randomQuote);
-// console.log(randomQuote.quote);
-
-
-// let codeBlock = ``;
-// codeBlock += `${quote}` + `${source}` + `${citation}` + `${year}`;
-
-
 
 
 /***
  * `printQuote` function
 ***/
-
-
 function printQuote() {
-  // 1. Create a variable that calls the getRandomQuote() 
-  // function
+  // Create a variable that calls the getRandomQuote() function
   const randomQuote = getRandomQuote();
-  // 2. Create a variable that initiates your HTML string with 
+
+  // Create a variable that initiates your HTML string with 
   // the first two <p></p> elements, their classNames, 
   // and the quote and source properties, but leave off 
   // the second closing `</p>` tag for now
-  // let html =`
-  //   <p class = "quote">${randomQuote.quote}</p><p class = "source">${randomQuote.source}<span class = "citation">${randomQuote.citation}</span><span class = "year">${randomQuote.year}</span></p>
-  //   `;
-
-  // html += `
-  //   <p class = "quote">${randomQuote.quote}</p>
-  //   <p class = "source">${randomQuote.source}
-  //     <span class = "citation">${randomQuote.citation}</span>
-  //     <span class = "year">${randomQuote.year}</span></p>
-  // `;
-
-  // 6. set the innerHTML of the quote-box div to equal the 
-  // complete HTML string
-  const quote = `<p class = "quote">${randomQuote.quote}</p>`;
-  const source = `<p class = "source">${randomQuote.source}`;
-  const citation = `<span class = "citation">${randomQuote.citation}</span>`;
-  const year = `<span class = "year">${randomQuote.year}</span></p>`;
+  let html = `
+    <p class = "quote">${randomQuote.quote}</p>
+    <p class = "source">${randomQuote.source}`;
   
-  let html = `${quote}` + `${source}` + `${citation}` + `${year}`;
+  // If the random quote object has a citation, year, or tag property, 
+  //concatenate a <span> element with the corresponding class to the HTML string.
+  if (randomQuote.citation) {
+    html += `<span class = "citation">${randomQuote.citation}</span>`;
+  };
+  if (randomQuote.year) {
+    html += `<span class = "year">${randomQuote.year}</span>`;
+  };
+  if (randomQuote.tag) {
+    html += `<span class = "tag">${randomQuote.tag}</span>`;
+  };
+  html += `</p></div>`;
 
+  // Set the innerHTML of the quote-box div to equal the complete HTML string
   document.querySelector('#quote-box').innerHTML = html;
-  console.log(html);
+
+  // Change the background color to a random color when a new quote prints to the page.
+  function randomRGB() {
+    const randomValue = () => Math.floor(Math.random() * 256);
+    const color = `rgb( ${randomValue()}, ${randomValue()}, ${randomValue()})`;
+    return color;
+  }
+  document.getElementById("backgroundchange").style.backgroundColor = randomRGB();
 }
 
 // printQuote()
-printQuote()
+printQuote();
+
+/*** 
+ create a timing function with the setInterval() method to print a 
+ new quote to the page at regular intervals (10 seconds). 
+***/
+setInterval(printQuote, 10000);
 
 
 /***
